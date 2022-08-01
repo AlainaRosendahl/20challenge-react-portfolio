c882e4b90956a5205695d009f2ad0f71453e7b0d
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-01 12:32:10 -0500

    header,footer & added needed components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import react from 'react'
+
+function Footer(){
+    return (
+        <div className="footer" id="footer">
+            <a href="https://github.com/AlainaRosendahl"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Github" className="icon"/></a>
+            <a href="https://www.linkedin.com/in/alaina-rosendahl-181b85236/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        </div>
+    )
+}
