1f6ee483c40f1c33179f4e14f8edd7cd5e16ad58
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-08-05 13:54:29 -0500

    resume, nav fix

---
+++ src/components/Resume/index.js
@@ -0,0 +1,46 @@
+import React from 'react'; 
+
+function Resume() {
+
+return(
+<section class="mb-5">
+    <h2 className="resume">Resume</h2>
+        <hr></hr>
+       
+           
+
+        <div class="row justify-content-center" id="resume">
+            <div class="mt-5 pl-5 pr-5">
+                <h2>Alaina Rosendahl</h2>
+            <ul>
+                    <li>Core competencies in full-stack web development.</li>
+                    <li>Self-driven team player with strengths in communication and developing interpersonal relationships.</li>
+                    <li>Possess excellent planning, organization, time management and decision-making skills.</li>
+                </ul>
+        
+
+                <p class="mt-5">
+                <a href="https://www.linkedin.com/in/alaina-rosendahl-181b85236/">
+                    <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="inkedIn"/></a>
+                </p>
+               
+               <a href="" class="link">Download Resume (not available yet)</a>
+               
+            </div>
+        </div>
+
+        <div class="justify-content-center mt-5">
+            <div>
+                <h2>Proficiencies</h2>
+                <p>HTML, CSS (Bootstrap, Foundation, HandleBars), JavaScript, jQuery, React.js, Angular.js, IndexedDB</p>
+                <p>Node.js, Express.js, SQL, Sequelize, NoSQL, APIs, Templating</p>
+                
+            </div>
+    
+            
+        </div>
+</section>
+);
+}
+
+export default Resume;
